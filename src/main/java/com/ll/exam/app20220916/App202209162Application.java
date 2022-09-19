986b6c46e40ad5297684edfccdc936b5d61663e8
nunusmohum <halojw301@gmail.com>
2022-09-19 13:13:14 +0900

    first setting

---
+++ src/main/java/com/ll/exam/app20220916/App202209162Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app20220916;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App202209162Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App202209162Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/app20220916/App202209162ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.exam.app20220916;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class App202209162ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
